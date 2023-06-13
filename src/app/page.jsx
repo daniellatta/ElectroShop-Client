af0292b8af02caa279a855715f2e9bfc0b896786
LeoSolari <leosolari9@gmail.com>
2023-06-13 12:57:35 -0300

    bajar esto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
-import LandingPage from '../components/LandingPage/LandingPage';
+import NavBar from "@/components/NavBar";
 
 export default function Landing() {
   return (
     <div>
-      <LandingPage />
+      <NavBar />
+      <h1 className="text-8xl bg-primary">Hola mundo</h1>
     </div>
   );
 }
--- src/components/LandingPage/LandingPage.jsx
+++
@@ -1,7 +0,0 @@
-export default function LandingPage() {
-  return (
-    <div>
-      <h1 className='text-4xl text-red-500'>Hola Mundo</h1>
-    </div>
-  );
-}
