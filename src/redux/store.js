f7f97ad1256cdc67e2a9ab20d39eb34991f6ba88
GoopyDev <alann.romero@gmail.com>
2023-06-20 09:38:31 -0300

    Se crea un nuevo reducer para el ShoppingCart. Se añade pequeño script para el uso de LocalStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import productSlice from '@/redux/features/searchBar';
+import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "@/redux/features/searchBar";
+import cartSlice from "@/redux/features/shoppingCart";
 
 export const store = configureStore({
   reducer: {
     items: productSlice,
+    shoppingCart: cartSlice,
   },
 });
