7b9925399f155784880acf857f8683c4113080fc
Andres Hernandez <hernandezboscanandre@gmail.com>
2023-06-22 02:31:41 -0400

    about us section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,11 @@
 
+import AboutUs from "@/components/AboutUs/AboutUs";
 
 export default function about() {
-  return <div>about</div>;
+  return (
+    <div className="flex items-center flex-col bg-[url('https://static.vecteezy.com/system/resources/previews/000/676/628/original/blue-blur-backdrop.jpg')] h-screen bg-cover bg-center">
+      <h1 className="my-4 text-blue-500 text-5xl tracking-wide font-extrabold uppercase">Who We Are</h1>
+      <AboutUs />
+    </div>
+  )
 }
