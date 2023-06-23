6f8de8014185c1dd605777fa04fa7f44262cce60
GoopyDev <alann.romero@gmail.com>
2023-06-23 11:32:28 -0300

    Se corrige flecha de cierre del carrito, y posición del contador de productos

--- src/app/CartProductsCount/CartProductsCount.jsx
+++ src/app/CartProductsCount/CartProductsCount.jsx
@@ -13,7 +13,7 @@ const CartProductsCount = () => {
   return (
     <div>
       {quantity > 0 && (
-        <div className="absolute z-[-1] top-[11px] right-[10px] flex justify-center items-center w-4 h-4 rounded-xl bg-red-500 text-white text-[8px] select-none">
+        <div className="absolute z-[-1] top-[11px] right-[7px] flex justify-center items-center w-4 h-4 rounded-xl bg-red-500 text-white text-[8px] select-none">
           {quantity}
         </div>
       )}
