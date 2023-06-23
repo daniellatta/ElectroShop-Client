bcf54c1e40cdb31de4629b4dcb9a8fdfcb3107d4
GoopyDev <alann.romero@gmail.com>
2023-06-23 00:26:48 -0300

    Se corrige posición de la flecha de cierre del carrito

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -56,10 +56,10 @@ const NavBar = () => {
             <CartContainer isOpen={isCartOpen} />
           </div>
           <div
-            className={`fixed z-[10002] top-[50px] right-[20px] flex justify-center text-white text-lg font-bold rounded border-2 border-white w-9 hover:bg-black/10 transition-all duration-300 ${
+            className={`fixed z-[10002] top-[50px] right-[20px] flex justify-center text-white text-lg font-bold shadow rounded border-2 border-white w-9 hover:bg-black/10 transition-all duration-300 ${
               isCartOpen
                 ? "translate-x-[0px]"
-                : "translate-x-full right-[-20px]"
+                : "translate-x-full right-[-40px]"
             }`}
             onClick={handleCartClick}
           >
