5104c52716681fa65cfc2baf333f1a30f85a35f0
daniellatta <danylatta1433@gmail.com>
2023-06-22 23:06:37 -0500

    Our Products and Filter Features

--- src/components/CardsContainer/CardsContainer.jsx
+++ src/components/CardsContainer/CardsContainer.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { fetchProducts } from '@/redux/features/products';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '../Card/Card';
 
@@ -9,19 +9,25 @@ export default function CardsContainer() {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.items);
   const byName = useSelector((state) => state.byName);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
 
-  let products = items.products;
-  if (byName.productsByName.length) products = byName.productsByName;
+  //Paginado
+  let products = [...items.products];
+  if (byName.productsByName.length) products = [...byName.productsByName];
+  if (items.productsByPrice.length) products = [...items.productsByPrice];
+  const lastIndex = currentPage * 9;
+  const firstIndex = lastIndex - 9;
+  let breedPage = products.slice(firstIndex, lastIndex);
+  //
   return (
-    <div>
+    <div className='flex flex-col w-full'>
       {items.loading && <p>Loading...</p>}
-
       <section className='grid grid-cols-3 gap-12'>
-        {products.map((product) => {
+        {breedPage.map((product) => {
           return (
             <section className='h-[300px] w-[300px]' key={product.id}>
               <Card
@@ -38,6 +44,19 @@ export default function CardsContainer() {
           );
         })}
       </section>
+      <footer className='flex justify-center gap-4'>
+        <button
+          onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)}>
+          prev
+        </button>
+        <p>{currentPage}</p>
+        <button
+          onClick={() =>
+            lastIndex <= products.length - 1 && setCurrentPage(currentPage + 1)
+          }>
+          next
+        </button>
+      </footer>
     </div>
   );
 }
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Pagination = () => {
-  return <div>Pagination</div>;
+const Pagination = ({ itemsPerPage, totalItems, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div>
+      
+    </div>
+  )
 };
 
 export default Pagination;
