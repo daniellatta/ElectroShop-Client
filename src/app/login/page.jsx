595cf929f8680d03abe1fd978165310bead4662f
LeoSolari <leosolari9@gmail.com>
2023-06-23 02:15:45 -0300

    hola

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,13 @@
-'use client';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from '../../redux/features/login';
+"use client";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../redux/features/login";
 
 const page = () => {
   const dispatch = useDispatch();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,21 +15,31 @@ const page = () => {
     dispatch(login(user));
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='Username'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type='password'
-        placeholder='Password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type='submit'>Login</button>
-    </form>
+    <div className="bg-black mt-16">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="ml-4"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onSubmit={handleSubmit} type="submit" className="text-white">
+          Login
+        </button>
+        {isAuthenticated ? (
+          <p className="text-16xl text-green-600">Hola estoy Logeado</p>
+        ) : (
+          <p className="text-red-600 text-16xl">Hola, No estoy logeado</p>
+        )}
+      </form>
+    </div>
   );
 };
 
