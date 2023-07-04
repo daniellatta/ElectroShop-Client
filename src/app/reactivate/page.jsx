5784ff0e73eedaa25d23392e25fa8f6474aaa442
LeoSolari <leosolari9@gmail.com>
2023-07-04 14:32:11 -0300

    leo

---
+++ src/app/reactivate/page.jsx
@@ -0,0 +1,85 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers, reactivateAccount } from "@/redux/features/adminDelete";
+import { login } from "../../redux/features/login";
+import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import "react-toastify/dist/ReactToastify.css";
+
+const page = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const users = useSelector((state) => state.adminDelete.users);
+  const userDni = JSON.parse(localStorage.getItem("id"));
+  const [userData, setUserData] = useState({
+    id: 0,
+    dni: "",
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+
+    const userInfo = users.find((user) => user.id === userDni);
+    if (userInfo) {
+      setUserData({
+        id: userInfo.id,
+        name: userInfo.name,
+        dni: userInfo.dni,
+        username: userInfo.username,
+        email: userInfo.email,
+      });
+    }
+  }, [dispatch, users]);
+
+  const handleReactivateUser = (userId) => {
+    dispatch(reactivateAccount(userId));
+    dispatch(login(userId));
+    localStorage.setItem("token", userData.token);
+    localStorage.setItem("email", userData.email);
+    localStorage.setItem("admin", userData.admin);
+    localStorage.setItem("active", true);
+    notifyActive();
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  };
+
+  const notifyActive = () => {
+    toast.success("Usuario REactivado correctamente", {
+      position: "top-center",
+      autoClose: 2000,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  };
+
+  return (
+    <div className="pt-20">
+      <div>
+        <div className="flex">
+          <p>Reactiva tu cuenta</p>
+          <button
+            onClick={() => handleReactivateUser(userData.id)}
+            className="bg-green-500 pl-4"
+          >
+            Reactivar
+          </button>
+        </div>
+        <div className="flex flex-col">
+          <p>Son estos tus datos?</p>
+          <p>{userData.name} </p>
+          <p>{userData.dni}</p>
+          <p>{userData.username}</p>
+          <p>{userData.email}</p>
+        </div>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default page;
--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,14 +1,14 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import React from "react";
 
 const Admin = () => {
   return (
-    <div className='flex relative'>
-      <div className='bg-[#333333] absolute h-96 w-48 top-20'>
-        <p className='uppercase text-[#00B8EA] py-4 px-3 text-xs'>main</p>
-        <div className='mx-3 text-[#fff]'>
+    <div className="flex relative">
+      <div className="bg-[#333333] absolute h-96 w-48 top-20">
+        <p className="uppercase text-[#00B8EA] py-4 px-3 text-xs">main</p>
+        <div className="mx-3 text-[#fff]">
           <p>Products</p>
-          <p>Users</p>
+          <Link href="/adminDelete">Manage users</Link>
         </div>
       </div>
     </div>
