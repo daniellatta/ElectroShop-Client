cfd22d0829260a27fb9e8721663898efcfe4ef2f
daniellatta <danylatta1433@gmail.com>
2023-07-06 02:04:42 -0500

    cositas

--- src/redux/features/adminDelete.js
+++ src/redux/features/adminDelete.js
@@ -39,11 +39,7 @@ const adminSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchUsers.pending, (state) => {
-        state.loading = true;
-      })
       .addCase(fetchUsers.fulfilled, (state, action) => {
-        state.loading = false;
         state.users = action.payload;
       })
       .addCase(fetchUsers.rejected, (state, action) => {
